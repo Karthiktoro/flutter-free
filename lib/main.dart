c52277e7e73451581a2ca8f52d1e0b0d565f6b75
toro <karthvc@gmail.com>
2023-08-25 18:48:43 +0530

    chapter3

--- lib/main.dart
+++ lib/main.dart
@@ -7,33 +7,86 @@ void main() {
   ));
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var myText = "enter my name";
+  final TextEditingController _nameController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Awesome App")),
       body: Center(
-        child: Container(
-          padding: EdgeInsets.all(8),
-          height: 100,
-          width: 100,
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-            color: Colors.amber,
-            borderRadius: BorderRadius.circular(10),
-            gradient: LinearGradient(colors: [Colors.pink, Colors.blue]),
-          ),
-          child: Text(
-            "I am a boxxx",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 15,
-              fontWeight: FontWeight.bold,
+          child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Card(
+            child: Column(
+              children: [
+                Image.asset("assets/photo-1580927752452-89d86da3fa0a.avif"),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(myText,
+                    style:
+                        TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextField(
+                    controller: _nameController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: "Enter the name",
+                      labelText: "Name",
+                    ),
+                  ),
+                )
+              ],
             ),
           ),
         ),
+      )),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.all(0),
+          children: [
+            UserAccountsDrawerHeader(
+                accountName: Text("Toro"),
+                accountEmail: Text("toro@gmail.com"),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      "https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80"),
+                )),
+            ListTile(
+              leading: Icon(Icons.person),
+              trailing: Icon(Icons.edit),
+              title: Text("Toro"),
+              subtitle: Text("Developer"),
+            ),
+            ListTile(
+              leading: Icon(Icons.email),
+              trailing: Icon(Icons.edit),
+              title: Text("Email"),
+              subtitle: Text("toro@gmail.com"),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          myText = _nameController.text;
+          setState(() {});
+        },
+        child: Icon(Icons.send),
       ),
     );
   }
