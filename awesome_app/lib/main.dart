b29ba1bdd2e989e4edcce5cfafa4068b2baed87c
toro <karthvc@gmail.com>
2023-08-29 16:58:00 +0530

    requesting http

---
+++ awesome_app/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:awesome_app/pages/homepage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+      theme: ThemeData(primarySwatch: Colors.green),
+    );
+  }
+}
---
+++ awesome_app/lib/pages/homepage.dart
@@ -0,0 +1,61 @@
+import 'package:awesome_app/drawer.dart';
+import 'package:awesome_app/name_card_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // var myText = "enter my name";
+  // final TextEditingController _nameController = TextEditingController();
+
+  var url = "https://jsonplaceholder.typicode.com/photos";
+  var data;
+  @override
+  void initState() {
+    fetchData();
+    super.initState();
+  }
+
+  fetchData() async {
+    var response = await http.get(Uri.parse(url));
+    data = jsonDecode(response.body);
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Awesome App")),
+      body: data != null
+          ? ListView.builder(
+              itemCount: data.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                    title: Text(data[index]["title"]),
+                    subtitle: Text("id : ${data[index]["id"]}"),
+                    leading: Image.network(data[index]["url"]));
+              },
+            )
+          : Center(child: CircularProgressIndicator()),
+      drawer: MyDrawer(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          //myText = _nameController.text;
+        },
+        child: Icon(Icons.send),
+      ),
+    );
+  }
+}
